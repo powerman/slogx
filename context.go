d11429c0361791a954100345d6195d62abbefdb0
tatyana <tatyana.krivonos@gmail.com>
2023-06-26 18:00:23 +0300

    chore(Ctx): Redact comment

--- context.go
+++ context.go
@@ -14,7 +14,7 @@ func NewContext(ctx context.Context, log *slog.Logger) context.Context {
 	return context.WithValue(ctx, contextKeyLog, log)
 }
 
-// FromContext returns the Logger value stored in ctx if exists or nil.
+// FromContext returns a Logger value stored in ctx if exists or nil.
 func FromContext(ctx context.Context) *slog.Logger {
 	log, _ := ctx.Value(contextKeyLog).(*slog.Logger)
 	return log

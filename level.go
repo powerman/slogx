9ebc174c076073227182a0b829cc0c6ff3382ca4
tatyana <tatyana.krivonos@gmail.com>
2023-06-29 14:38:22 +0300

    chore: Add ParseLevel

---
+++ level.go
@@ -0,0 +1,23 @@
+package slogx
+
+import (
+	"log/slog"
+	"strings"
+)
+
+// ParseLevel convert levelName from flag or config file into slog.Level.
+func ParseLevel(levelName string) slog.Level {
+	switch strings.ToLower(levelName) {
+	case "err", "error", "fatal", "crit", "critical", "alert", "emerg", "emergency":
+		return slog.LevelError
+	case "wrn", "warn", "warning":
+		return slog.LevelWarn
+	case "inf", "info", "notice":
+		return slog.LevelInfo
+	case "dbg", "debug", "trace":
+		return slog.LevelDebug
+	default:
+		slog.Debug("failed", "levelName", levelName) // Will be changed to use custom DefaultLogger.
+		return slog.LevelDebug
+	}
+}

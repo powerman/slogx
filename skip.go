28a6db5565bdb5513cac6379968d6da7e60e0a77
tatyana <tatyana.krivonos@gmail.com>
2023-07-12 00:41:49 +0300

    add comments

--- skip.go
+++ skip.go
@@ -7,6 +7,10 @@ import (
 	"time"
 )
 
+// LogSkip writes the location of called function to a record,
+// skiping it given times and calls Handle() on the handler.
+// Value skip=0 works exactly like slog.Log(),
+// value skip=1 skips caller of LogSkip().
 func LogSkip(ctx context.Context, skip int, handler slog.Handler, level slog.Level, msg string, args ...any) {
 	var pcs [1]uintptr
 	runtime.Callers(1+skip, pcs[:])
@@ -15,6 +19,10 @@ func LogSkip(ctx context.Context, skip int, handler slog.Handler, level slog.Lev
 	_ = handler.Handle(ctx, r)
 }
 
+// LogAttrsSkip writes the location of called function to a record,
+// skiping it given times and calls Handle() on the handler.
+// Value skip=0 works exactly like slog.Log(),
+// value skip=1 skips caller of LogSkip().
 func LogAttrsSkip(ctx context.Context, skip int, handler slog.Handler, level slog.Level, msg string, attrs ...slog.Attr) {
 	var pcs [1]uintptr
 	runtime.Callers(1+skip, pcs[:])

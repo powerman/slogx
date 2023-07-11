865ac805c207be04161d9c3afa3d3da87392e86c
tatyana <tatyana.krivonos@gmail.com>
2023-07-12 00:46:17 +0300

    fix tests

--- skip_test.go
+++ skip_test.go
@@ -19,7 +19,7 @@ func TestLogSkip(tt *testing.T) {
 	var buf bytes.Buffer
 	ctx := context.Background()
 	slogx.LogSkip(ctx, 0, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(8), "message", "err", io.EOF)
-	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip.go:12 msg=message err=EOF")
+	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip.go:16 msg=message err=EOF")
 
 	slogx.LogSkip(ctx, 1, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(8), "message", "err", io.EOF)
 	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip_test.go:24 msg=message err=EOF")
@@ -32,7 +32,7 @@ func TestLogAttrsSkip(tt *testing.T) {
 	var buf bytes.Buffer
 	ctx := context.Background()
 	slogx.LogAttrsSkip(ctx, 0, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(0), "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
-	t.Match(buf.String(), "level=INFO source=.*/slogx/skip.go:20 msg=message ID=18")
+	t.Match(buf.String(), "level=INFO source=.*/slogx/skip.go:28 msg=message ID=18")
 
 	slogx.LogAttrsSkip(ctx, 1, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(0), "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
 	t.Match(buf.String(), "level=INFO source=.*/slogx/skip_test.go:37 msg=message ID=18")

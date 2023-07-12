58a08e91ab69b8632d738d473acfcd10d666a306
tatyana <tatyana.krivonos@gmail.com>
2023-07-12 12:57:53 +0300

    feat: Add handler to ctx

--- context.go
+++ context.go
@@ -7,15 +7,29 @@ import (
 
 type contextKey int
 
-const contextKeyLog contextKey = 0
+const (
+	contextKeyLog contextKey = iota
+	contextKeyHandler
+)
 
-// NewContext returns a new Context that carries value log.
-func NewContext(ctx context.Context, log *slog.Logger) context.Context {
+// NewContext returns a new Context that carries value handler.
+func NewContext(ctx context.Context, handler slog.Handler) context.Context {
+	return context.WithValue(ctx, contextKeyHandler, handler)
+}
+
+// FromContext returns a Handler value stored in ctx if exists or nil.
+func FromContext(ctx context.Context) slog.Handler {
+	handler, _ := ctx.Value(contextKeyHandler).(slog.Handler)
+	return handler
+}
+
+// NewContextWithLogger returns a new Context that carries value log.
+func NewContextWithLogger(ctx context.Context, log *slog.Logger) context.Context {
 	return context.WithValue(ctx, contextKeyLog, log)
 }
 
-// FromContext returns a Logger value stored in ctx if exists or nil.
-func FromContext(ctx context.Context) *slog.Logger {
+// LoggerFromContext returns a Logger value stored in ctx if exists or nil.
+func LoggerFromContext(ctx context.Context) *slog.Logger {
 	log, _ := ctx.Value(contextKeyLog).(*slog.Logger)
 	return log
 }

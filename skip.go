e81493fa592888abe993165aedecbe8c62673417
tatyana <tatyana.krivonos@gmail.com>
2023-07-12 22:49:05 +0300

    fixes

--- skip.go
+++ skip.go
@@ -7,26 +7,38 @@ import (
 	"time"
 )
 
-// LogSkip writes the location of called function to a record,
-// skiping it given times and calls Handle() on the handler.
-// Value skip=0 works exactly like slog.Log(),
-// value skip=1 skips caller of LogSkip().
+// LogSkip emits a log record using handler with the
+// current time and the given level and message.
+// Value skip=0 works exactly like (*slog.Logger).Log,
+// value skip=1 skips caller of LogSkip() etc.
 func LogSkip(ctx context.Context, skip int, handler slog.Handler, level slog.Level, msg string, args ...any) {
+	if !handler.Enabled(ctx, level) {
+		return
+	}
 	var pcs [1]uintptr
-	runtime.Callers(1+skip, pcs[:])
+	runtime.Callers(2+skip, pcs[:])
 	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
 	r.Add(args...)
+	if ctx == nil {
+		ctx = context.Background()
+	}
 	_ = handler.Handle(ctx, r)
 }
 
-// LogAttrsSkip writes the location of called function to a record,
-// skiping it given times and calls Handle() on the handler.
-// Value skip=0 works exactly like slog.Log(),
-// value skip=1 skips caller of LogSkip().
+// LogAttrsSkip emits a log record using handler with the
+// current time and the given level and message.
+// Value skip=0 works exactly like (*slog.Logger).Log,
+// value skip=1 skips caller of LogSkip() etc.
 func LogAttrsSkip(ctx context.Context, skip int, handler slog.Handler, level slog.Level, msg string, attrs ...slog.Attr) {
+	if !handler.Enabled(ctx, level) {
+		return
+	}
 	var pcs [1]uintptr
-	runtime.Callers(1+skip, pcs[:])
+	runtime.Callers(2+skip, pcs[:])
 	r := slog.NewRecord(time.Now(), level, msg, pcs[0])
 	r.AddAttrs(attrs...)
+	if ctx == nil {
+		ctx = context.Background()
+	}
 	_ = handler.Handle(ctx, r)
 }
--- skip_test.go
+++ skip_test.go
@@ -19,10 +19,11 @@ func TestLogSkip(tt *testing.T) {
 	var buf bytes.Buffer
 	ctx := context.Background()
 	slogx.LogSkip(ctx, 0, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(8), "message", "err", io.EOF)
-	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip.go:16 msg=message err=EOF")
+	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip_test.go:21 msg=message err=EOF")
 
+	buf.Truncate(0)
 	slogx.LogSkip(ctx, 1, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(8), "message", "err", io.EOF)
-	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip_test.go:24 msg=message err=EOF")
+	t.Match(buf.String(), "level=ERROR source=.*/testing/testing.go:1595 msg=message err=EOF")
 }
 
 func TestLogAttrsSkip(tt *testing.T) {
@@ -32,8 +33,9 @@ func TestLogAttrsSkip(tt *testing.T) {
 	var buf bytes.Buffer
 	ctx := context.Background()
 	slogx.LogAttrsSkip(ctx, 0, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(0), "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
-	t.Match(buf.String(), "level=INFO source=.*/slogx/skip.go:28 msg=message ID=18")
+	t.Match(buf.String(), "level=INFO source=.*/slogx/skip_test.go:35 msg=message ID=18")
 
+	buf.Truncate(0)
 	slogx.LogAttrsSkip(ctx, 1, slog.NewTextHandler(&buf, &slog.HandlerOptions{AddSource: true}), slog.Level(0), "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
-	t.Match(buf.String(), "level=INFO source=.*/slogx/skip_test.go:37 msg=message ID=18")
+	t.Match(buf.String(), "level=INFO source=.*/testing/testing.go:1595 msg=message ID=18")
 }

bbf67871f182320b0fff86a889d2e8441561aafe
tatyana <tatyana.krivonos@gmail.com>
2023-07-13 13:17:48 +0300

    remove individual test data

--- stack_test.go
+++ stack_test.go
@@ -14,9 +14,9 @@ func TestStack(tt *testing.T) {
 	stack := slogx.Stack()
 	t.DeepEqual(stack.Key, slogx.KeyStack)
 	t.Match(stack.Value, "github.com/powerman/slogx_test.TestStack")
-	t.Match(stack.Value, "/home/tatyana/projects/slogx/stack_test.go:14")
+	t.Match(stack.Value, "/slogx/stack_test.go:14")
 	t.Match(stack.Value, "testing.tRunner")
-	t.Match(stack.Value, "/home/tatyana/sdk/go1.21rc2/src/testing/testing.go:1595")
+	t.Match(stack.Value, "/testing/testing.go:1595")
 	t.Match(stack.Value, "created by testing.(.*).Run in goroutine 1")
-	t.Match(stack.Value, "/home/tatyana/sdk/go1.21rc2/src/testing/testing.go:1648")
+	t.Match(stack.Value, "/testing/testing.go:1648")
 }

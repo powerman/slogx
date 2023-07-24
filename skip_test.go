0e0c7dd52fccaf2da4aef4349ef75f09359e4a49
tatyana <tatyana.krivonos@gmail.com>
2023-07-24 18:37:23 +0300

    tmp generate

--- skip_test.go
+++ skip_test.go
@@ -1,5 +1,7 @@
 package slogx_test
 
+//go:generate mockgen -package=slogx -source=XXX/log/slog/handler.go -destination=mock.handler.go Handler
+
 import (
 	"bytes"
 	"context"
@@ -7,6 +9,7 @@ import (
 	"log/slog"
 	"testing"
 
+	gomock "github.com/golang/mock/gomock"
 	"github.com/powerman/check"
 
 	"github.com/powerman/slogx"
@@ -23,8 +26,10 @@ func TestLogSkip(tt *testing.T) {
 	slogx.LogSkip(ctx, 0, h, slog.LevelWarn, "message", "err", io.EOF)
 	t.Len(buf.String(), 0)
 
+	t.Panic(func() { slogx.LogSkip(ctx, 0, nil, slog.LevelError, "message", "err", io.EOF) })
+
 	slogx.LogSkip(ctx, 0, h, slog.LevelError, "message", "err", io.EOF)
-	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip_test.go:26 msg=message err=EOF")
+	t.Match(buf.String(), "level=ERROR source=.*/slogx/skip_test.go:[0-9]+ msg=message err=EOF")
 
 	buf.Truncate(0)
 	slogx.LogSkip(nil, 1, h, slog.LevelError, "message", "err", io.EOF)
@@ -42,10 +47,38 @@ func TestLogAttrsSkip(tt *testing.T) {
 	slogx.LogAttrsSkip(ctx, 0, h, slog.LevelInfo, "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
 	t.Len(buf.String(), 0)
 
+	t.Panic(func() {
+		slogx.LogAttrsSkip(ctx, 0, nil, slog.LevelWarn, "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
+	})
+
 	slogx.LogAttrsSkip(ctx, 0, h, slog.LevelWarn, "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
-	t.Match(buf.String(), "level=WARN source=.*/slogx/skip_test.go:45 msg=message ID=18")
+	t.Match(buf.String(), "level=WARN source=.*/slogx/skip_test.go:[0-9]+ msg=message ID=18")
 
 	buf.Truncate(0)
 	slogx.LogAttrsSkip(nil, 1, h, slog.LevelWarn, "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
 	t.Match(buf.String(), "level=WARN source=.*/testing/testing.go:[0-9]+ msg=message ID=18")
+}
+
+func TestLogSkipCtx(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+
+	h := slogx.NewMockHandler(ctrl)
+	ctx := context.Background()
+	h.EXPECT().Enabled(ctx, slog.LevelError).Return(true)
+	h.EXPECT().Handle(ctx, gomock.Any()).Return(nil)
+	slogx.LogSkip(nil, 0, h, slog.LevelError, "message", "err", io.EOF)
+}
+
+func TestLogAttrsSkipCtx(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+	ctrl := gomock.NewController(t)
+
+	h := slogx.NewMockHandler(ctrl)
+	ctx := context.Background()
+	h.EXPECT().Enabled(ctx, slog.LevelWarn).Return(true)
+	h.EXPECT().Handle(ctx, gomock.Any()).Return(nil)
+	slogx.LogAttrsSkip(nil, 1, h, slog.LevelWarn, "message", slog.Attr{Key: "ID", Value: slog.IntValue(18)})
 }

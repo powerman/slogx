982694d59b11cf9ebc7211529f8c81929fcde566
tatyana <tatyana.krivonos@gmail.com>
2023-08-04 21:53:26 +0300

    fix generate

--- tools.go
+++ tools.go
@@ -1,10 +1,10 @@
-//go:build generate
+//go:build tools
 
 //go:generate mkdir -p .buildcache/bin
 //go:generate -command GOINSTALL env "GOBIN=$PWD/.buildcache/bin" go install
 
 package tools
 
+//go:generate GOINSTALL github.com/golang/mock/mockgen@v1.6.0
 //go:generate GOINSTALL github.com/golangci/golangci-lint/cmd/golangci-lint@v1.52.0
 //go:generate GOINSTALL github.com/mattn/goveralls@v0.0.12
-//go:generate GOINSTALL github.com/golang/mock/mockgen@v1.6.0

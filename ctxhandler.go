7c7012a1566a7dd3f132c158866c9fc12fcb4cdc
tatyana <tatyana.krivonos@gmail.com>
2023-08-11 17:17:04 +0300

    chore: Add CtxHandler realisation

--- ctxhandler.go
+++ ctxhandler.go
@@ -5,13 +5,15 @@ import (
 	"log/slog"
 )
 
+const KeyBadCtx = "!BADCTX"
+
 // CtxHandler is used as a default logger.
 // It applies for applications only (not for libraries).
 //
 // Usually we used logger stored in ctx. We had to extract it first.
 // It took us one extra line in every function:
 //
-//	log := structlog.FromContext(s.ctx, nil)
+//	log := structlog.FromContext(ctx, nil)
 //	log.Info("some message",...)
 //
 // With CtxHandler we minimise it.
@@ -27,17 +29,17 @@ import (
 // We recommend not to add attributes/groups on global logger.
 // All Attributes and Groups you should apply on handler, stored in ctx:
 //
-//	handler = handler.WithGroup("solar")
-//	handler = handler.WithAttrs([]slog.Attr{slog.Int("earth", 3)})
+//	handler = handler.WithGroup("g")
+//	handler = handler.WithAttrs([]slog.Attr{slog.Int("key", 3)})
 //	ctx = slogx.NewContext(ctx, handler)
-//	slog.InfoContext(ctx, "greet")
+//	slog.InfoContext(ctx, "message")
 //
 // Spawning a new logger using With or WithGrop will cause these settings
 // to be applied after the settings of handler stored in ctx:
 //
-//	log := slog.New(slog.NewTextHandler(os.Stderr, nil)).With(slog.Int("top", 20))
+//	log := slog.With(slog.Int("top", 20))
 //
-//	handler.WithAttrs([]slog.Attr{slog.Int("top", 10)})
+//	handler = handler.WithAttrs([]slog.Attr{slog.Int("top", 10)})
 //	log.InfoContext(slogx.NewContext(ctx, handler), "list")
 //
 // Output:
@@ -46,11 +48,19 @@ import (
 //
 // By convention such logger must not be carried by stack neither in ctx nor in parameters.
 //
-// CtxHandler optionally reports !BADCTX with the address of current ctx
-// if there is no handler in it.
+// CtxHandler optionally reports !BADCTX with ctx as a value if there is no handler in it.
 type CtxHandler struct {
-	ignoreBADCTX bool
 	handler      slog.Handler
+	opList       []data
+	ignoreBADCTX bool
+}
+
+// Data is used for store operations with logger.
+// It may be group to add or attributes. It also keeps an order.
+// Applying Handle method all data will be added to a handler at last.
+type data struct {
+	group string
+	attrs []slog.Attr
 }
 
 // CtxHandlerOption is an option for a CtxHandler.
@@ -59,33 +69,83 @@ type ctxHandlerOption func(*CtxHandler)
 // SetDefaultCtxHandler sets a CtxHandler as a default logger.
 // It applies given options. If opts is nil, the default options are used.
 func SetDefaultCtxHandler(fallback slog.Handler, opts ...ctxHandlerOption) {
-	panic("TODO")
+	const size = 64 << 10
+	ctxHandler := &CtxHandler{
+		handler: fallback,
+		opList:  make([]data, size),
+	}
+	for _, opt := range opts {
+		opt(ctxHandler)
+	}
+	slog.SetDefault(slog.New(ctxHandler))
 }
 
 // Enabled works as (slog.Handler).Enabled.
-// It uses handler returned by FromContext or fallback handler.
+// It useys handler returned by FromContext or fallback handler.
 func (h *CtxHandler) Enabled(ctx context.Context, l slog.Level) bool {
-	panic("TODO")
+	handler := FromContext(ctx)
+	if handler == nil {
+		handler = h.handler
+	}
+	return handler.Enabled(ctx, l)
 }
 
 // Handle works as (slog.Handler).Handler.
 // It uses handler returned by FromContext or fallback handler.
 // Optionally add !BADCTX attr if FromContext returns nil.
 func (h *CtxHandler) Handle(ctx context.Context, r slog.Record) error {
-	panic("TODO")
+	handler := FromContext(ctx)
+	if handler == nil {
+		handler = h.handler
+		if !h.ignoreBADCTX {
+			handler = handler.WithAttrs([]slog.Attr{slog.Any(KeyBadCtx, ctx)})
+		}
+	}
+	for _, op := range h.opList {
+		if len(op.group) > 0 {
+			handler = handler.WithGroup(op.group)
+		} else {
+			handler = handler.WithAttrs(op.attrs)
+		}
+	}
+	return handler.Handle(ctx, r)
 }
 
 // WithAttrs works exactly like (slog.Handler).WithAttrs.
 func (h *CtxHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
-	panic("TODO")
+	if len(attrs) == 0 {
+		return h
+	}
+	ctxHandler := h.clone()
+	ctxHandler.opList = append(ctxHandler.opList, data{
+		attrs: attrs,
+	})
+	return ctxHandler
 }
 
 // WithGroup works exactly like (slog.Handler).WithGroup.
 func (h *CtxHandler) WithGroup(name string) slog.Handler {
-	panic("TODO")
+	if len(name) == 0 {
+		return h
+	}
+	ctxHandler := h.clone()
+	ctxHandler.opList = append(ctxHandler.opList, data{
+		group: name,
+	})
+	return ctxHandler
 }
 
 // LaxCtxHandler is an option for disable adding !BADCTX attr.
 func LaxCtxHandler() ctxHandlerOption {
-	panic("TODO")
+	return func(ctxHandler *CtxHandler) {
+		ctxHandler.ignoreBADCTX = true
+	}
+}
+
+func (h *CtxHandler) clone() *CtxHandler {
+	return &CtxHandler{
+		handler:      h.handler,
+		opList:       h.opList,
+		ignoreBADCTX: h.ignoreBADCTX,
+	}
 }
---
+++ ctxhandler_test.go
@@ -0,0 +1,85 @@
+package slogx_test
+
+import (
+	"bytes"
+	"context"
+	"log/slog"
+	"os"
+	"testing"
+
+	"github.com/powerman/check"
+
+	"github.com/powerman/slogx"
+)
+
+func TestEnabled(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+
+	h := slog.NewTextHandler(os.Stdout, nil)
+	slogx.SetDefaultCtxHandler(h)
+	t.True(slog.Default().Enabled(context.Background(), slog.LevelWarn))
+
+	h = slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelError})
+	ctx := slogx.NewContext(context.Background(), h)
+	t.True(slog.Default().Enabled(ctx, slog.LevelError))
+}
+
+func TestCtxHandler(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+
+	var buf bytes.Buffer
+	var h slog.Handler
+	// With TextHandler
+	h = slog.NewTextHandler(&buf, nil).WithGroup("g").WithAttrs([]slog.Attr{slog.String("key1", "value1"), slog.String("key2", "value2")})
+	ctx := slogx.NewContext(context.Background(), h)
+	slogx.SetDefaultCtxHandler(slog.NewTextHandler(&buf, nil))
+	slog.InfoCtx(ctx, "some message")
+	t.Match(buf.String(), `level=INFO msg="some message" g.key1=value1 g.key2=value2`)
+
+	buf.Truncate(0)
+	log := slog.With(slog.String("key1", "modified"))
+	log.InfoCtx(ctx, "some message")
+	t.Match(buf.String(), `level=INFO msg="some message" g.key1=value1 g.key2=value2 g.key1=modified`)
+
+	buf.Truncate(0)
+	log = log.WithGroup("g2").With(slog.String("key3", "value3"))
+	log.InfoCtx(ctx, "some message")
+	t.Match(buf.String(), `level=INFO msg="some message" g.key1=value1 g.key2=value2 g.key1=modified g.g2.key3=value3`)
+
+	// With JsonHandler
+	buf.Truncate(0)
+	h = slog.NewJSONHandler(&buf, nil).WithGroup("g").WithAttrs([]slog.Attr{slog.String("key1", "value1"), slog.String("key2", "value2")})
+	ctx = slogx.NewContext(context.Background(), h)
+	slogx.SetDefaultCtxHandler(slog.NewJSONHandler(&buf, nil))
+	slog.InfoCtx(ctx, "some message")
+	t.Match(buf.String(), `"level":"INFO","msg":"some message","g":{"key1":"value1","key2":"value2"}}`)
+
+	buf.Truncate(0)
+	log = slog.With(slog.String("key1", "modified"))
+	log.InfoCtx(ctx, "some message")
+	t.Match(buf.String(), `"level":"INFO","msg":"some message","g":{"key1":"value1","key2":"value2","key1":"modified"}}`)
+
+	buf.Truncate(0)
+	log = log.WithGroup("g2").With(slog.String("key3", "value3"))
+	log.InfoCtx(ctx, "some message")
+	t.Match(buf.String(), `"level":"INFO","msg":"some message","g":{"key1":"value1","key2":"value2","key1":"modified","g2":{"key3":"value3"}}}`)
+}
+
+func TestLaxCtxHandler(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+
+	var buf bytes.Buffer
+	ctx := context.Background()
+	h := slog.NewTextHandler(&buf, nil).WithAttrs([]slog.Attr{slog.String("key1", "value1")})
+	slogx.SetDefaultCtxHandler(h)
+	slog.InfoCtx(ctx, "some message")
+	t.Match(buf.String(), `level=INFO msg="some message" key1=value1 !BADCTX`)
+
+	buf.Truncate(0)
+	slogx.SetDefaultCtxHandler(h, slogx.LaxCtxHandler())
+	slog.InfoCtx(ctx, "some message")
+	t.NotMatch(buf.String(), "!BADCTX")
+}

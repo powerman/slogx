2ec499f7aab370d1b00db86bb72b2c2c7efb30bf
tatyana <tatyana.krivonos@gmail.com>
2024-01-04 19:22:19 +0200

    resolve

--- error.go
+++ error.go
@@ -5,8 +5,6 @@ import (
 	"log/slog"
 )
 
-const KeyBadKey = "!BADKEY"
-
 type errorAttrs struct { //nolint:errname // Custom naming.
 	err   error
 	attrs []slog.Attr
@@ -90,38 +88,4 @@ func getAllAttrs(attrs []slog.Attr, err error) []slog.Attr {
 		}
 	}
 	return attrs
-}
-
-func argsToAttrSlice(args []any) []slog.Attr {
-	var (
-		attr  slog.Attr
-		attrs []slog.Attr
-	)
-	for len(args) > 0 {
-		attr, args = argsToAttr(args)
-		attrs = append(attrs, attr)
-	}
-	return attrs
-}
-
-// argsToAttr turns a prefix of the nonempty args slice into an Attr
-// and returns the unconsumed portion of the slice.
-// If args[0] is an Attr, it returns it.
-// If args[0] is a string, it treats the first two elements as
-// a key-value pair.
-// Otherwise, it treats args[0] as a value with a missing key.
-func argsToAttr(args []any) (slog.Attr, []any) { // Probably will be add with CtxHandler for common use.
-	switch x := args[0].(type) {
-	case string:
-		if len(args) == 1 {
-			return slog.String(KeyBadKey, x), nil
-		}
-		return slog.Any(x, args[1]), args[2:]
-
-	case slog.Attr:
-		return x, args[1:]
-
-	default:
-		return slog.Any(KeyBadKey, x), args[1:]
-	}
 }
--- error_test.go
+++ error_test.go
@@ -15,6 +15,8 @@ func TestErrorAttrs(tt *testing.T) {
 	t := check.T(tt)
 	t.Parallel()
 
+	const badKey = "!BADKEY"
+
 	var (
 		e              = "new error"
 		key            = "Key"
@@ -26,7 +28,7 @@ func TestErrorAttrs(tt *testing.T) {
 
 		newErrorBadKey     = slogx.NewError(err, "key1")
 		newErrorBadKeyAttr = slog.Any("", newErrorBadKey)
-		groupValueBadKey   = slog.GroupValue(slog.String(slogx.KeyBadKey, "key1"), slog.String("", newErrorBadKey.Error()))
+		groupValueBadKey   = slog.GroupValue(slog.String(badKey, "key1"), slog.String("", newErrorBadKey.Error()))
 
 		newErrorAttr     = slogx.NewError(err, slog.Int("key", 3))
 		newErrorAttrAttr = slog.Any("", newErrorAttr)
@@ -34,7 +36,7 @@ func TestErrorAttrs(tt *testing.T) {
 
 		newErrorInt         = slogx.NewError(err, 8)
 		newErrorIntAttr     = slog.Any("", newErrorInt)
-		groupValueBadKeyInt = slog.GroupValue(slog.Any(slogx.KeyBadKey, 8), slog.String("", newErrorInt.Error()))
+		groupValueBadKeyInt = slog.GroupValue(slog.Any(badKey, 8), slog.String("", newErrorInt.Error()))
 
 		strAttr    = slog.String("key", "value")
 		anyAttr    = slog.Any("key", complex(2.2, 2.7))

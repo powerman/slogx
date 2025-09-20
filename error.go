c67276dcf7a7444c0cde1c698151724ada8a8e8b
salamandra19 <tatyana.krivonos@gmail.com>
2025-09-20 11:02:03 +0300

    feat: Add errorAttrs type (#42)
    closes #10
    
    ---------
    
    Co-authored-by: Alex Efros <powerman@powerman.name>

---
+++ error.go
@@ -0,0 +1,77 @@
+package slogx
+
+import (
+	"errors"
+	"log/slog"
+)
+
+type errorAttrs struct { //nolint:errname // Custom naming.
+	err   error
+	attrs []slog.Attr
+}
+
+// Error implements error interface.
+func (e errorAttrs) Error() string { return e.err.Error() }
+
+// Unwrap returns wrapped error.
+func (e errorAttrs) Unwrap() error { return e.err }
+
+type errorAttrsConfig struct{}
+
+type errorAttrsOption func(*errorAttrsConfig)
+
+// NewError returns err with attached slog attrs specified by args.
+func NewError(err error, args ...any) error {
+	return NewErrorAttrs(err, argsToAttrSlice(args)...)
+}
+
+// NewErrorAttrs returns err with attached slog attrs.
+func NewErrorAttrs(err error, attrs ...slog.Attr) error {
+	if err == nil {
+		return nil
+	}
+	return errorAttrs{err: err, attrs: attrs}
+}
+
+// ErrorAttrs returns an slog.ReplaceAttr function that will replace attr's Value of error type
+// with slog.GroupValue containing all attrs attached to any of recursively unwrapped errors
+// plus original attr's Value (error).
+//
+// By default returned attr's Key depends on groups:
+// if groups are empty then Key will be empty, otherwise Key will be attr's Key.
+// This behaviour may be changed by given options.
+//
+// If attr's Value is not of error type or error has no attached attrs then returns original attr.
+func ErrorAttrs(_ ...errorAttrsOption) func(groups []string, attr slog.Attr) slog.Attr {
+	return func(groups []string, a slog.Attr) slog.Attr {
+		if a.Value.Kind() != slog.KindAny {
+			return a
+		}
+		err, ok := a.Value.Any().(error)
+		if !ok {
+			return a
+		}
+
+		attrs := getAllAttrs(err)
+		if len(attrs) == 0 {
+			return a
+		}
+		attrs = append(attrs, slog.String(a.Key, a.Value.String()))
+
+		var key string
+		if len(groups) > 0 {
+			key = a.Key
+		}
+		return slog.Attr{Key: key, Value: slog.GroupValue(attrs...)}
+	}
+}
+
+func getAllAttrs(err error) []slog.Attr {
+	if err == nil {
+		return nil
+	}
+	if errAttr, ok := err.(errorAttrs); ok { //nolint:errorlint // Necessary type assertion.
+		return append(errAttr.attrs, getAllAttrs(errors.Unwrap(err))...)
+	}
+	return getAllAttrs(errors.Unwrap(err))
+}
---
+++ error_test.go
@@ -0,0 +1,72 @@
+package slogx_test
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"testing"
+
+	"github.com/powerman/check"
+
+	"github.com/powerman/slogx"
+)
+
+func TestErrorAttrs(tt *testing.T) {
+	t := check.T(tt)
+	t.Parallel()
+
+	const badKey = "!BADKEY"
+
+	var (
+		e              = "new error"
+		key            = "Key"
+		group          = []string{"group"}
+		err            = errors.New(e) //nolint:err113 // False positive.
+		newError       = slogx.NewError(err, "key1", "value1", "key2", "value2")
+		newErrorAttrs  = slogx.NewErrorAttrs(newError, slog.Int("key3", 3), slog.Int("key4", 4))
+		errorAttrsFunc = slogx.ErrorAttrs()
+
+		newErrorBadKey     = slogx.NewError(err, "key1")
+		newErrorBadKeyAttr = slog.Any("", newErrorBadKey)
+		groupValueBadKey   = slog.GroupValue(slog.String(badKey, "key1"), slog.String("", newErrorBadKey.Error()))
+
+		newErrorAttr     = slogx.NewError(err, slog.Int("key", 3))
+		newErrorAttrAttr = slog.Any("", newErrorAttr)
+		groupValueAttr   = slog.GroupValue(slog.Int("key", 3), slog.String("", newErrorAttr.Error()))
+
+		newErrorInt         = slogx.NewError(err, 8)
+		newErrorIntAttr     = slog.Any("", newErrorInt)
+		groupValueBadKeyInt = slog.GroupValue(slog.Any(badKey, 8), slog.String("", newErrorInt.Error()))
+
+		strAttr    = slog.String("key", "value")
+		anyAttr    = slog.Any("key", complex(2.2, 2.7))
+		errAttr    = slog.Any("key", err)
+		groupValue = slog.GroupValue(slog.Int("key3", 3), slog.Int("key4", 4), slog.Any("key1", "value1"), slog.Any("key2", "value2"), slog.String(key, "new error"))
+
+		wrapedError1 = fmt.Errorf("error1: %w", err)
+		wrapedError2 = fmt.Errorf("error2: %w", newError)
+		wrapedError3 = fmt.Errorf("error3: %w", newErrorAttrs)
+		value1       = slog.AnyValue(wrapedError1)
+		groupValue2  = slog.GroupValue(slog.Any("key1", "value1"), slog.Any("key2", "value2"), slog.String(key, "error2: new error"))
+		groupValue3  = slog.GroupValue(slog.Int("key3", 3), slog.Int("key4", 4), slog.Any("key1", "value1"), slog.Any("key2", "value2"), slog.String(key, "error3: new error"))
+	)
+
+	t.DeepEqual(slogx.NewError(nil), nil)
+	t.DeepEqual(slogx.NewErrorAttrs(nil), nil)
+
+	t.DeepEqual(slogx.NewError(err).Error(), e)
+	t.DeepEqual(errorAttrsFunc(nil, newErrorBadKeyAttr), slog.Attr{Key: "", Value: groupValueBadKey})
+	t.DeepEqual(errorAttrsFunc(nil, newErrorAttrAttr), slog.Attr{Key: "", Value: groupValueAttr})
+	t.DeepEqual(errorAttrsFunc(nil, newErrorIntAttr), slog.Attr{Key: "", Value: groupValueBadKeyInt})
+
+	t.DeepEqual(errorAttrsFunc(nil, strAttr), strAttr)
+	t.DeepEqual(errorAttrsFunc(nil, anyAttr), anyAttr)
+	t.DeepEqual(errorAttrsFunc(nil, errAttr), errAttr)
+
+	t.DeepEqual(errorAttrsFunc(nil, slog.Any(key, newErrorAttrs)), slog.Attr{Key: "", Value: groupValue})
+	t.DeepEqual(errorAttrsFunc(group, slog.Any(key, newErrorAttrs)), slog.Attr{Key: key, Value: groupValue})
+
+	t.DeepEqual(errorAttrsFunc(nil, slog.Any(key, wrapedError1)), slog.Attr{Key: key, Value: value1})
+	t.DeepEqual(errorAttrsFunc(nil, slog.Any(key, wrapedError2)), slog.Attr{Key: "", Value: groupValue2})
+	t.DeepEqual(errorAttrsFunc(group, slog.Any(key, wrapedError3)), slog.Attr{Key: key, Value: groupValue3})
+}
